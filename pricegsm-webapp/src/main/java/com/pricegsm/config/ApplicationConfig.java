cb6cedbe9bafd3a5f62d6d398fdd99133edf8679
Andrey Bugaev <a.bugaev@gastrofix.com>
2014-01-17 14:14:26 +0300

    tuned log

--- pricegsm-webapp/src/main/java/com/pricegsm/config/ApplicationConfig.java
+++ pricegsm-webapp/src/main/java/com/pricegsm/config/ApplicationConfig.java
@@ -1,27 +1,29 @@
 package com.pricegsm.config;
 
-import static org.springframework.context.annotation.ComponentScan.Filter;
-
+import com.pricegsm.Application;
 import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.stereotype.Controller;
 
-import com.pricegsm.Application;
+import static org.springframework.context.annotation.ComponentScan.Filter;
 
 @EnableScheduling
 @Configuration
 @ComponentScan(basePackageClasses = Application.class, excludeFilters = @Filter({Controller.class, Configuration.class}))
 class ApplicationConfig {
-	
-	@Bean
-	public static PropertyPlaceholderConfigurer propertyPlaceholderConfigurer() {
-		PropertyPlaceholderConfigurer ppc = new PropertyPlaceholderConfigurer();
-		ppc.setLocation(new ClassPathResource("/persistence.properties"));
-		return ppc;
-	}
-	
+
+    @Bean
+    public static PropertyPlaceholderConfigurer propertyPlaceholderConfigurer() {
+        PropertyPlaceholderConfigurer ppc = new PropertyPlaceholderConfigurer();
+        ppc.setLocations(new Resource[]{
+                new ClassPathResource("/persistence.properties"),
+                new ClassPathResource("/build.properties")});
+        return ppc;
+    }
+
 }
