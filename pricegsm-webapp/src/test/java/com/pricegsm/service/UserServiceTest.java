3bc617634a6da7994c10e7674d4f3f8504c78b98
Andrey Bugaev <a.bugaev@gastrofix.com>
2014-01-28 12:50:15 +0300

    added tests

--- pricegsm-webapp/src/test/java/com/pricegsm/service/UserServiceTest.java
+++ pricegsm-webapp/src/test/java/com/pricegsm/service/UserServiceTest.java
@@ -1,16 +1,8 @@
-package com.pricegsm.controller.account;
-
-import static org.fest.assertions.Assertions.assertThat;
-import static org.mockito.Matchers.*;
-import static org.mockito.Mockito.*;
-
-import java.util.Collection;
+package com.pricegsm.service;
 
 import com.pricegsm.dao.BaseUserDao;
 import com.pricegsm.domain.Administrator;
 import com.pricegsm.domain.BaseUser;
-import com.pricegsm.service.BaseUserService;
-import com.pricegsm.service.BaseUserServiceImpl;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
@@ -21,6 +13,11 @@ import org.mockito.runners.MockitoJUnitRunner;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.Collection;
+
+import static org.fest.assertions.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class UserServiceTest {
