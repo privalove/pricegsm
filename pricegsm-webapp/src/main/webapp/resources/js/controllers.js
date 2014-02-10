085d54a1fb9ac99d9e89eb555303585641094a79
Andrey Bugaev <a.bugaev@gastrofix.com>
2014-02-10 14:17:07 +0300

    small fix

--- pricegsm-webapp/src/main/webapp/resources/js/controllers.js
+++ pricegsm-webapp/src/main/webapp/resources/js/controllers.js
@@ -11,7 +11,7 @@ function IndexCtrl($scope, $cookieStore, $filter, $locale, indexResource, IndexR
 
         var exclude = $scope.product.excludeQuery ? "~~(" + $scope.product.excludeQuery.replace(/,/g, "|") + ")" : "";
 
-        var query = "(" + $scope.product.searchQuery + ")(" + $scope.product.colorQuery.replace(/,/g, "|") + ")" + exclude;
+        var query = "(" + $scope.product.searchQuery.replace(/,/g, "|") + ")(" + $scope.product.colorQuery.replace(/,/g, "|") + ")" + exclude;
 
         return "http://market.yandex.ru/search.xml?hid=" + $scope.product.type.yandexId + "&text=" + encodeURIComponent(query) + "&nopreciser=1&how=aprice&np=1&onstock=1";
     };
